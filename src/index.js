72a1094eea4fd5979c3829b418c18efcce86971d
Robin Wieruch <wrobin@gmx.net>
2018-06-19 16:51:49 +0800

    include dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const foo = { foo: 'foo' };
 const bar = { bar: 'bar' };
 const fooBar = { ...foo, ...bar };
 
 console.log(fooBar);
+
+console.log(process.env.SOME_ENV_VARIABLE);
